8dbcf6a111b6b12ddec1a24877e5c0ed4e51ba8f
sirius1618 <sagittarius_a1.618@proton.me>
2026-02-12 12:42:09 -0300

    feat: Repository passageiro

--- src/main/java/com/github/sirius1618/transportadora_carros/repository/PassageiroRepository.java
+++ src/main/java/com/github/sirius1618/transportadora_carros/repository/PassageiroRepository.java
@@ -1,4 +1,9 @@
 package com.github.sirius1618.transportadora_carros.repository;
 
-public interface PassageiroRepository {
+import com.github.sirius1618.transportadora_carros.model.Passageiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassageiroRepository extends JpaRepository <Passageiro, Long> {
 }

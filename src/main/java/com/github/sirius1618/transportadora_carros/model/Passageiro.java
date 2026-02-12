dd128fb93ba7470855e37b0298651135a05dd68a
sirius1618 <sagittarius_a1.618@proton.me>
2026-02-12 12:41:51 -0300

    feat: Entidade passageiro

--- src/main/java/com/github/sirius1618/transportadora_carros/model/Passageiro.java
+++ src/main/java/com/github/sirius1618/transportadora_carros/model/Passageiro.java
@@ -1,4 +1,32 @@
 package com.github.sirius1618.transportadora_carros.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.Generated;
+
+@Entity
 public class Passageiro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    String nome;
 }

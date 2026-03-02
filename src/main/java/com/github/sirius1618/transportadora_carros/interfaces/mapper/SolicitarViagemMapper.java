c374f9d7ad6bc38547ed920e38d4bca8ac7f0346
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-02 16:42:57 -0300

    feat(SolicitarViagemMapper): Mapea um input para um service de SolicitacaoViagem

--- src/main/java/com/github/sirius1618/transportadora_carros/interfaces/mapper/SolicitarViagemMapper.java
+++ src/main/java/com/github/sirius1618/transportadora_carros/interfaces/mapper/SolicitarViagemMapper.java
@@ -1,4 +1,29 @@
-package com.github.sirius1618.transportadora_carros.interfaces.dto.input;
+package com.github.sirius1618.transportadora_carros.interfaces.mapper;
+
+import com.github.sirius1618.transportadora_carros.interfaces.dto.input.SolicitarViagemInput;
+import com.github.sirius1618.transportadora_carros.model.Passageiro;
+import com.github.sirius1618.transportadora_carros.model.SolicitacaoViagem;
+import com.github.sirius1618.transportadora_carros.repository.PassageiroRepository;
+import com.github.sirius1618.transportadora_carros.repository.SolicitacaoViagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class SolicitarViagemMapper {
+
+    @Autowired
+    PassageiroRepository passageiroRepository;
+
+    public SolicitacaoViagem map (SolicitarViagemInput input) {
+
+        Passageiro passageiro = passageiroRepository.findById(input.passageiroId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        SolicitacaoViagem solicitacaoViagem = new SolicitacaoViagem();
+        solicitacaoViagem.setPassageiro(passageiro);
+        solicitacaoViagem.setOrigem(input.destino());
+        solicitacaoViagem.setOrigem(input.origem());
+
+        return solicitacaoViagem;
+    }
 }

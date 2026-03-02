99522e9a80aecdaa7e718e29c27867886c0724d1
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-02 13:19:52 -0300

    chore: Tira importações de bibliotecas desnecessarias

--- src/main/java/com/github/sirius1618/transportadora_carros/repository/MotoristaRepository.java
+++ src/main/java/com/github/sirius1618/transportadora_carros/repository/MotoristaRepository.java
@@ -1,7 +1,6 @@
 package com.github.sirius1618.transportadora_carros.repository;
 
 import com.github.sirius1618.transportadora_carros.model.Motorista;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
